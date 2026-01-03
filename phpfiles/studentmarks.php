adacdd821dea5a6f47ceead1434e4c62f671bdcf
Syed Hasnain Raza <149717902+Hasnainvoker@users.noreply.github.com>
2026-01-03 10:24:12 +0530

    added php files

---
+++ phpfiles/studentmarks.php
@@ -0,0 +1,22 @@
+
+<?php
+// Student details
+$rollNo = 101;
+$name = "Raza";
+
+// Marks of three subjects
+$mark1 = 75;
+$mark2 = 80;
+$mark3 = 85;
+
+// Calculate total marks
+$total = $mark1 + $mark2 + $mark3;
+
+// Display details
+echo "Roll Number: " . $rollNo . "<br>";
+echo "Name: " . $name . "<br>";
+echo "Marks 1: " . $mark1 . "<br>";
+echo "Marks 2: " . $mark2 . "<br>";
+echo "Marks 3: " . $mark3 . "<br>";
+echo "Total Marks: " . $total;
+?>
