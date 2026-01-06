56e56f1c913628ad4d3a7013db0b885a831f4368
Kaushiki <nayakkaushiki19@gmail.com>
2026-01-06 19:05:42 +0530

    modified 6-jan task added few page styling

--- phptasks/6-jan_task.php
+++ phptasks/6-jan_task.php
@@ -5,32 +5,56 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h2>Employee Details</h2>
+    <header>
+        <h2>Employee Details</h2>
+    </header>
 
-    <?php
-    $Employee = [
-        [
-            "name" => "ABC",
-            "basicSalary" => 50000
-        ],
-        
-        [
-            "name" => "XYZ",
-            "basicSalary" => 70000
-        ]
-    ];
+    <main>
+        <?php
+        echo "<div class = 'ab'>";
+        $Employee = [
+            [
+                "name" => "Emp1",
+                "basicSalary" => 50000
+            ],
+            
+            [
+                "name" => "Emp2",
+                "basicSalary" => 70000
+            ],
 
-    foreach ($Employee as $emp){
-        $HRA = 0.2 * $emp["basicSalary"];
-        $DA = 0.1 * $emp["basicSalary"];
-        $totalSalary = $emp["basicSalary"] + $HRA + $DA;
-        echo "<b>","Employee Name : ".$emp["name"],"</b>"."<br>";
-        echo "Basic salary : ".$emp["basicSalary"]."<br>";
-        echo "HRA : ".$HRA."<br>";
-        echo "DA : ".$DA."<br>";
-        echo "Total Salary : ".$totalSalary."<br>";
-        echo "<hr>";
-    }
-    ?>
+            [
+                "name" => "Emp3",
+                "basicSalary" => 50000
+            ],
+
+            [
+                "name" => "Emp4",
+                "basicSalary" => 50000
+            ],
+
+            [
+                "name" => "Emp5",
+                "basicSalary" => 50000
+            ],
+        ];
+
+        foreach ($Employee as $emp){
+            echo "<span class = 'a'>";
+            $HRA = 0.2 * $emp["basicSalary"];
+            $DA = 0.1 * $emp["basicSalary"];
+            $totalSalary = $emp["basicSalary"] + $HRA + $DA;
+            echo "<b>Employee Name : ".$emp["name"]."</b><br>";
+            echo "Basic salary : ".$emp["basicSalary"]."<br>";
+            echo "HRA : ".$HRA."<br>";
+            echo "DA : ".$DA."<br>";
+            echo "<span class='total'>Total Salary : ".$totalSalary."</span><br>";
+            echo "</span>";
+            // echo "<hr>";
+            
+        }
+        echo "</span>";
+        ?>
+    </main>
 </body>
 </html>
