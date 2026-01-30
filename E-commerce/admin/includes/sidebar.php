5f9e872b154d24ea453dccfae82911a67304f3ab
Kaushiki <nayakkaushiki19@gmail.com>
2026-01-30 19:56:54 +0530

    project updates

---
+++ E-commerce/admin/includes/sidebar.php
@@ -0,0 +1,9 @@
+<div class="sidebar">
+    <h2>Admin Panel</h2>
+    <a href="index1.php">Dashboard</a>
+    <a href="products/list.php">Products</a>
+    <a href="users/list.php">Users</a>
+    <a href="orders/list.php">Orders</a>
+    <a href="payments/list.php">Payments</a>
+    <a href="logout.php">Logout</a>
+</div>
